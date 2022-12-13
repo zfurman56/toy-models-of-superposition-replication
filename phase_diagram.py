01ec7fa2ee09cc0df2eb2dc14256dc407207697e
Zach Furman <zach.furman1@gmail.com>
2022-12-13 18:51:40 -0500

    Fix outdated phase diagram code

--- phase_diagram.py
+++ phase_diagram.py
@@ -18,39 +18,36 @@ class ParallelReLUModels(nn.Module):
     def forward(self, x):
         return F.relu((x @ torch.transpose(self.W, -1, -2) @ self.W) + self.b.unsqueeze(3))
 
-def generate_sparse_data(averaging_dims, density_dims, importance_dims, feature_size, batch_size, densities):
-    mask = torch.rand(averaging_dims, density_dims, importance_dims, batch_size, feature_size) < densities[:, None, None, None]
-    return torch.rand(averaging_dims, density_dims, importance_dims, batch_size, feature_size) * mask
+def generate_sparse_data(averaging_dims, importance_dims, feature_size, batch_size, densities, device):
+    mask = torch.rand(averaging_dims, len(densities), importance_dims, batch_size, feature_size, device=device) < densities[:, None, None, None]
+    return torch.rand(averaging_dims, len(densities), importance_dims, batch_size, feature_size, device=device) * mask
 
 def importance_weighted_MSE(x_prime, x, importances):
-    return torch.mean(torch.square(x_prime - x) * importances.unsqueeze(1), dim=-1)
+    return torch.mean(torch.square(x_prime - x) * importances.unsqueeze(1), dim=[-1, -2])
 
-def train(model, averaging_dims, densities, rel_importances, feature_size, batch_size):
+def train(model, averaging_dims, densities, rel_importances, feature_size, batch_size, device):
     loss_fn = importance_weighted_MSE
     optimizer = torch.optim.AdamW(list(model.parameters()), lr=0.001)
     density_dims = len(densities)
     importance_dims = len(rel_importances)
 
     # create importances from relative importances
-    importances = torch.stack((torch.ones(importance_dims), rel_importances), dim=1)
+    importances = torch.stack((torch.ones(importance_dims, device=device), rel_importances), dim=1)
 
     iterations = 5000
     for t in range(iterations):
-        data = generate_sparse_data(averaging_dims, density_dims, importance_dims, feature_size, batch_size, densities)
+        data = generate_sparse_data(averaging_dims, importance_dims, feature_size, batch_size, densities, device)
 
         pred = model(data)
-        losses = loss_fn(pred, data, importances)
-        loss = torch.mean(losses)
+        loss = torch.mean(loss_fn(pred, data, importances))
 
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
 
         if (t % 100) == 0:
-            print(f"Iteration {t+1}\n-------------------------------")
             print(f"loss: {loss.item():>7f}")
-
-    return losses
+            print(f"Iteration {t+1}\n-------------------------------")
 
 # x coordinate makes color go from red to blue, y makes color get whiter
 def color_map_2d(x, y):
@@ -63,9 +60,20 @@ def color_map_2d(x, y):
 
     return mpl.colors.hsv_to_rgb((hue, y, 1))
 
-def create_plots(model, losses, densities, rel_importances, feature_size):
+def create_plots(model, densities, rel_importances, feature_size, averaging_dims):
+    model = model.to('cpu')
     W = model.W.detach()
-    weighting = F.normalize(1/losses, p=1, dim=0)
+
+    test_batch = generate_sparse_data(averaging_dims, len(rel_importances), feature_size, 8192, densities.cpu(), 'cpu')
+    importances = torch.stack((torch.ones(len(rel_importances)), rel_importances.cpu()), dim=1)
+
+    model.eval()
+    with torch.no_grad():
+        test_preds = model(test_batch)
+
+    # use losses to weight result average (lower loss gets weighted higher)
+    losses = importance_weighted_MSE(test_batch, test_preds, importances)
+    weighting = F.normalize(1 / (losses.cpu() ** 2), p=1, dim=0).unsqueeze(-1)
 
     feature_norms = torch.linalg.norm(W, dim=-2)
 
@@ -73,19 +81,15 @@ def create_plots(model, losses, densities, rel_importances, feature_size):
     W_normalized = W / torch.linalg.norm(W, dim=-2, keepdim=True)
     feature_superpositions = torch.sum(torch.square((W_normalized.transpose(-1, -2) @ W) * mask), dim=-1)
 
-    feature_norms_avg = torch.sum(weighting * feature_norms, dim=0)
-    feature_superpositions_avg = torch.mean(weighting * feature_superpositions, dim=0)
+    feature_norms_avg = torch.sum(feature_norms * weighting, dim=0)
+    feature_superpositions_avg = torch.sum(feature_superpositions * weighting, dim=0)
 
     image = np.zeros((len(densities), len(rel_importances), 3))
     for d in range(len(densities)):
         for r in range(len(rel_importances)):
             image[d, r, :] = color_map_2d(feature_superpositions_avg[d, r, 0], feature_norms_avg[d, r, 0])
 
-    plt.imshow(image)
-    plt.matshow(feature_norms_avg[:, :, 0])
-    plt.matshow(feature_superpositions_avg[:, :, 0])
-    plt.matshow(feature_norms_avg[:, :, 1])
-    plt.matshow(feature_superpositions_avg[:, :, 1])
+    plt.imshow(image, origin='lower')
     plt.show()
 
 
@@ -93,28 +97,21 @@ def create_plots(model, losses, densities, rel_importances, feature_size):
 torch.manual_seed(1)
 np.random.seed(1)
 
+device = 'cuda'
 feature_size = 2
 activation_size = 1
 batch_size = 1024
 averaging_dims = 10
 # density goes from 1/100 to 1
-densities = 10 ** torch.linspace(-2, 0, 20)
+densities = 10 ** torch.linspace(-2, 0, 50, device=device)
 # relative importance goes from 1/10X to 10X
-rel_importances = 10 ** torch.linspace(-1, 1, 20)
+rel_importances = 10 ** torch.linspace(-1, 1, 50, device=device)
 
 
-model = ParallelReLUModels(averaging_dims, len(densities), len(rel_importances), feature_size, activation_size)
+model = ParallelReLUModels(averaging_dims, len(densities), len(rel_importances), feature_size, activation_size).to(device)
 
-losses = train(model, averaging_dims, densities, rel_importances, feature_size, batch_size)
+train(model, averaging_dims, densities, rel_importances, feature_size, batch_size, device)
 print("Done!")
 
-create_plots(model, losses, densities, rel_importances, feature_size)
-
-#m = np.zeros((100, 100, 3))
-#for i in range(100):
-#    for j in range(100):
-#        m[i, j, :] = color_map_2d(i/99, j/99)
-
-#plt.imshow(m)
-#plt.show()
+create_plots(model, densities, rel_importances, feature_size, averaging_dims)
 
